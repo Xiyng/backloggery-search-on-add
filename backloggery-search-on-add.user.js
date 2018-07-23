57b68728b46f2f5261473123d4f86a75a4e47753
Xiyng <xiyng.dev@gmail.com>
2018-07-23 22:39:00 +0300

    Add basic search functionality

---
+++ backloggery-search-on-add.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name     Backloggery Search on Add
+// @author   Xiyng
+// @version  0.1
+// @grant    none
+// @include  https://www.backloggery.com/newgame.php?user=*
+// @run-at   document-idle
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+'use strict';
+
+run();
+
+function run() {
+    const addGameButton = document.body.querySelector('input[value="Add Game"]');
+    const addGameButtonContainer = addGameButton.parentNode;
+    const searchButton = $('<input type="button" value="Search">')[0];
+    addGameButtonContainer.prepend(searchButton);
+    searchButton.addEventListener('click', search);
+}
+
+function search() {
+    const gameName = getGameName();
+    if (gameName.length < 1) {
+        return;
+    }
+    const uriGameName = encodeURIComponent(gameName);
+    const userName = getUserName();
+    window.open(
+        'https://www.backloggery.com/games.php?user=' + userName + '&search=' + uriGameName,
+        '_blank'
+    );
+}
+
+function getGameName() {
+    const nameInput = document.body.querySelector('input[name="name"]');
+    return nameInput.value;
+}
+
+function getUserName() {
+    const parameters = getHrefParameterString();
+    const userName = getParameterFromParameterString(parameters, 'user');
+    return userName;
+}
+
+function getHrefParameterString() {
+    const href = window.location.href;
+    const tokens = href.split('?');
+    const parameters = tokens[1];
+    return parameters;
+}
+
+function getParameterFromParameterString(parameterString, parameterName) {
+    const parameters = parameterString.split('&');
+    const wantedParameter = parameters.find(parameter => parameter.startsWith(parameterName + '='));
+    const parameterValue = wantedParameter.substring(wantedParameter.indexOf('=') + 1);
+    return parameterValue;
+}
